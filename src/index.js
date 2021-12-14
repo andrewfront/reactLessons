e76c788ccd21bbc97d62c384f65e16ccac73b9a5
Andrew <andrew.ndrw1@gmail.com>
2021-12-14 19:29:43 +0300

    secondCommit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import ReactDOM from 'react-dom'
+// function Greeting() {
+//   return <h4>This is Andrew and this is my first component</h4>
+// }
+function Greeting() {
+  return (
+    <div>
+      <h1>This is Andrew and this is my first component</h1>
+    </div>
+  )
+}
+// const Greeting = () => {
+//   return React.createElement('div', {}, React.createElement('h1', {}, 'Hello World'))
+// }
+ReactDOM.render(<Greeting/>, document.getElementById('root'))
